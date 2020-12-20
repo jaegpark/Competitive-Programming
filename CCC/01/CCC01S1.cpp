b05bed30966ecaa67ac5a52c9efd1634af1aeaa8
thejammerr <jaepark.g@gmail.com>
2020-12-20 01:56:03 -0500

    solve CCC01

---
+++ CCC/01/CCC01S1.cpp
@@ -0,0 +1,133 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using db = double;
+using str = string;
+//TODO: doesn't work for first test case 
+#define pb push_back
+#define pf push_front
+#define mp make_pair
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    str input = "";
+    int count = 0;
+    int sum = 0;
+    cin >> input;
+    char letters[input.length()+1];
+    
+
+    strcpy(letters, input.c_str());
+    int cur = 0, cardCount=0;
+    cout << "Cards Dealt Points" << endl;
+    while (cur != input.length())
+    {
+        char i = input[cur];
+        switch (i)
+        {
+            // point values based on card
+            case 'A':
+            {
+                count += 4;
+                cardCount++;
+                cout << i<<" ";
+                break;
+            }
+            case 'K':
+            {
+                count += 3;
+                cardCount++;
+                cout << i<<" ";
+                break;
+            }
+            case 'Q':
+            {
+                count += 2;
+                cardCount++;
+                cout << i<<" ";
+                break;
+            }
+            case 'J':
+            {
+                count += 1;
+                cardCount++;
+                cout << i<<" ";
+                break;
+            }
+            //case work for new line (output points)
+            case 'C':
+            {
+                if (cardCount==0){
+                    count+=3;
+                }else if (cardCount==1){
+                    count+=2;
+                }else if (cardCount==2){
+                    count+=1;
+                }
+                cout << "Clubs ";
+                count = 0;
+                cardCount= 0;
+                break;
+            }
+            case 'D':
+            {
+                if (cardCount==0){
+                    count+=3;
+                }else if (cardCount==1){
+                    count+=2;
+                }else if (cardCount==2){
+                    count+=1;
+                }
+                sum+=count;
+                cout << count << "\n"<< "Diamonds ";
+                count = 0;
+                cardCount= 0;
+                break;
+            }
+            case 'H':
+            {
+                if (cardCount==0){
+                    count+=3;
+                }else if (cardCount==1){
+                    count+=2;
+                }else if (cardCount==2){
+                    count+=1;
+                }
+                sum+=count;
+                cout << count << "\n" << "Hearts ";
+                count = 0;
+                cardCount= 0;
+                break;
+            }
+            case 'S':
+            {
+                if (cardCount==0){
+                    count+=3;
+                }else if (cardCount==1){
+                    count+=2;
+                }else if (cardCount==2){
+                    count+=1;
+                }
+                sum+=count;
+                cout << count << "\n"<< "Spades ";
+                count = 0;
+                cardCount=0;
+                break;
+            }
+            default: {
+                cout<<i<<" ";
+                cardCount++;
+                break;
+            }
+        }
+        cur++;
+    }
+    sum+=count;
+    cout<<count<<endl;
+    cout<<"Total "<<sum;
+}
--- templates/template.cpp
+++ templates/template.cpp
@@ -10,16 +10,10 @@ using str = string;
 #define pf push_front
 #define mp make_pair
 
-int s, t;
-int dates[5][7];
-
-
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    cin >> s >> t;
     
-
 }
