bd5acc210111fe9d0341ca99cb4272da315f934e
thejammerr <jaepark.g@gmail.com>
2020-12-22 01:13:06 -0500

    solved ccc15s1

--- CCC/15/CCC15S2.cpp
+++ CCC/15/CCC15S2.cpp
@@ -15,23 +15,8 @@ int main()
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    stack<int> s;
-    int n, sum=0;
-    cin>>n;
-    for (int x, i = 0; i < n; i++)
-    {
-        cin>>x;
-        if (x==0){
-            s.pop();
-        }else {
-            s.push(x);
-        }
-    }
-    while(!s.empty()){
-        sum += s.top();
-        s.pop();
-    }
-    cout<<sum;
+
+    stack<pair<int, char>> in;
     
     
 }
