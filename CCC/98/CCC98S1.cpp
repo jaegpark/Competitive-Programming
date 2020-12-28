d117879df0d63c8c2494cfa304279f79907c80e5
thejammerr <jaepark.g@gmail.com>
2020-12-28 17:30:06 -0500

    changed the includes so it's cross compatible

--- CCC/98/CCC98S1.cpp
+++ CCC/98/CCC98S1.cpp
@@ -1,6 +1,35 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <cstring>
+
 
 using namespace std;
+
+using ll = long long;
+using ld = long double;
+using db = double;
+using str = string;
 
 int main()
 {
